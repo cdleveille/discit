dcdae02efbe498c292a6d82e0bf6c93db84ac838
Chris Leveille <cdleveille@gmail.com>
2021-12-09 23:40:28 -0500

    new logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header: React.FC = () => {
 	return (
 		<div className="header">
 			<a href="https://github.com/cdleveille/discit" target="_blank" rel="noreferrer">
-				<span className="emoji">🥏</span> DiscIt
+				<img src="disc_golf.svg"></img>&nbsp;&nbsp;&nbsp;DiscIt&nbsp;&nbsp;
 			</a>
 		</div>
 	);
