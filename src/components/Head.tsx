e3c8d51e673584b6ede1c0a6f60dcf5fcc8a9d7a
Chris Leveille <cdleveille@gmail.com>
2022-03-08 15:44:49 -0500

    better icons

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,10 +5,13 @@ export const HeadContent: React.FC = () => {
 	return (
 		<Head>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-			<link
-				rel="icon"
-				href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥏</text></svg>"
-			/>
+			<link rel="manifest" href="/manifest.json" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/icon-180x180.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png" />
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
+			<meta name="msapplication-TileColor" content="#ffffff" />
+			<meta name="theme-color" content="#ffffff" />
 			<title>DiscIt</title>
 		</Head>
 	);
