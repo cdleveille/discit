54613a831e222a5ea42d718f437a6d9c68d60813
Chris Leveille <cdleveille@gmail.com>
2025-05-05 14:31:47 -0400

    remove throw error on server if api key not set

--- src/server/helpers/config.ts
+++ src/server/helpers/config.ts
@@ -6,10 +6,6 @@ const PORT = Bun.env.VITE_PORT ? Number.parseInt(Bun.env.VITE_PORT) : 3000;
 
 const API_KEY = Bun.env.VITE_API_KEY;
 
-if (!API_KEY) {
-	throw new Error("Required env var VITE_API_KEY is not set");
-}
-
 export const Config = {
 	IS_PROD,
 	PORT,
